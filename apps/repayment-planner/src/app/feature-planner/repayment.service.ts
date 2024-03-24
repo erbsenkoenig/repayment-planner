1be49e3bf8f062a78cfb0c14e3adb0677b1f5100
Stephanie Katterwe <stephanie.katterwe@dpdhl.com>
2024-03-24 20:33:08 +0100

    fix(planner): fix last rate
    * last rate adjusts in case loan is payed off before fixed interest rate is complete

--- apps/repayment-planner/src/app/feature-planner/repayment.service.ts
+++ apps/repayment-planner/src/app/feature-planner/repayment.service.ts
@@ -27,7 +27,7 @@ export class RepaymentService {
     // push initial payment
     result.push({
       id: 0,
-      residualDept: loanAmount * -1,
+      residualDept: loanAmount,
       interest: 0,
       rate: loanAmount * -1,
       repayment: loanAmount * -1,
@@ -50,31 +50,29 @@ export class RepaymentService {
     for (let i = 2; i <= amountOfPayments; i++) {
       // get prev residual dept for interest calculation
       const prevResidualDept = result[i - 1].residualDept;
-      const prevRepaymentRate = result[i - 1].repayment;
-
-      // if the prev residual dept was smaller than the repayment rate
-      // the last payment was the last
-      if (prevResidualDept < prevRepaymentRate) {
-        break;
-      }
 
       const interest = (prevResidualDept / (100 * 12)) * debitInterest;
       const repayment = rate - interest;
 
       // new residualDept is prev minus curr repayment
       let residualDept = prevResidualDept - repayment;
+      let currRate = rate;
       // if the result would be below 0 the current repayment is just what we have left
-      if (residualDept < 0) {
-        residualDept = prevResidualDept;
+      if (residualDept < repayment) {
+        currRate = residualDept;
       }
 
       result.push({
         id: i,
-        residualDept,
+        residualDept: residualDept,
         interest,
         repayment,
-        rate,
+        rate: currRate,
       });
+
+      if (residualDept < repayment) {
+        break;
+      }
     }
 
     return result;
