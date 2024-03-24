3637877e637133011c01c1a19eacb5d2e4682bea
Stephanie Katterwe <stephanie.katterwe@dpdhl.com>
2024-03-24 15:36:47 +0100

    feat(core): initial setup (cont)
    - setup tailwind
    - remove sample code

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './node_modules/david-ui-angular/**/*.{html,ts,js,mjs}',
+    './apps/**/src/**/*.{html,ts,js}',
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
